6892d234e9318217ffaa6a55a2384adbe71bdb0a
Jungack <57200786+Jungack@users.noreply.github.com>
2022-03-26 20:33:04 +0100

    Added control of motor SG90

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Création de la navigation de base avec onglet en bas de l'app
 https://www.section.io/engineering-education/bottom-navigation-bar-in-android/
 Ce lien pourra être réutilisé pour ajouter de nouveaux onglets à l'application.
+
+# Dialogue avec Arduino
+Utilisation de la bibliothèque https://github.com/OmarAflak/Arduino-Library
+Cf settings.gradle et build.gradle Module pour rajouter cette librairie. Pas de modif de build.gradle Project pour rajouter cette bibliothèque.
--- app/src/main/java/com/example/roboticarmhandapp/Hand.java
+++ app/src/main/java/com/example/roboticarmhandapp/Hand.java
@@ -1,64 +1,114 @@
 package com.example.roboticarmhandapp;
 
+import android.hardware.usb.UsbDevice;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Hand#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Hand extends Fragment {
+import java.math.BigInteger;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import me.aflak.arduino.Arduino;
+import me.aflak.arduino.ArduinoListener;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+public class Hand extends Fragment implements ArduinoListener {
+
+    SeekBar HandSeekBar;
+    TextView HandAngleIndicator;
+    private Arduino arduino;
+    byte seeked;
+    byte[] sendSeeked = new byte[1];
 
     public Hand() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Hand.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Hand newInstance(String param1, String param2) {
-        Hand fragment = new Hand();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_hand, container, false);
+        View v = inflater.inflate(R.layout.fragment_hand, container, false);
+
+        // Declaration des views
+        HandSeekBar = v.findViewById(R.id.HandSeekBar);
+        HandAngleIndicator = v.findViewById(R.id.HandAngleIndicator);
+        arduino = new Arduino(getActivity());
+        arduino.addVendorId(2341);
+
+        HandSeekBar.setMax(180);
+        HandSeekBar.setProgress(90);
+
+        HandAngleIndicator.setText("Bougez moi !");
+
+        HandSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                int adjust = (int) (i / 2);
+                int newi = adjust * 2;
+                HandSeekBar.setProgress(newi);
+                seeked = (byte) adjust; // byte prend des valeurs entre -128 et 127
+                sendSeeked[0] = seeked;
+                HandAngleIndicator.setText("Angle du moteur : " + newi);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                arduino.send(sendSeeked);
+            }
+        });
+
+        return v;
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        arduino.setArduinoListener(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        arduino.unsetArduinoListener();
+        arduino.close();
+    }
+
+    @Override
+    public void onArduinoAttached(UsbDevice device) {
+        HandAngleIndicator.setText("Arduino connecté !");
+        arduino.open(device);
+    }
+
+    @Override
+    public void onArduinoDetached() {
+        HandAngleIndicator.setText("Arduino déconnecté");
+    }
+
+    @Override
+    public void onArduinoMessage(byte[] bytes) {
+
+    }
+
+    @Override
+    public void onArduinoOpened() {
+
+    }
+
+    @Override
+    public void onUsbPermissionDenied() {
+        HandAngleIndicator.setText("Permission denied. Attempting again in 3 sec...");
+        new Handler().postDelayed(() -> arduino.reopen(), 3000);
+    }
+
 }
